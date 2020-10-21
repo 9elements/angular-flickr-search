7b7112d2c06eba68c83a971a239628749435b62e
Mathias Schaefer (molily) <molily@mailbox.org>
2020-10-21 23:48:14 +0200

    Improve screenshot on failure plugin

--- e2e/src/screenshot-on-failure-plugin.ts
+++ e2e/src/screenshot-on-failure-plugin.ts
@@ -2,6 +2,8 @@
 import * as fs from 'fs';
 import { browser } from 'protractor';
 
+const DIR = './e2e/screenshots/';
+
 // Adapted from
 // https://github.com/testing-angular-applications/testing-angular-applications/blob/master/chapter10/test_screenshot/screenshot_on_failure_plugin.ts
 // MIT-licensed by Craig Nishina
@@ -9,8 +11,12 @@ export async function postTest(passed: boolean, testInfo: any): Promise<void> {
   if (!passed) {
     const category = testInfo.category.replace(/ /g, '_');
     const name = testInfo.name.replace(/ /g, '_');
-    const fileName = `${category}_${name}_failure.png`;
+    const path = `${DIR}${category}_${name}_failure.png`;
     const data = await browser.takeScreenshot();
-    fs.writeFileSync(fileName, data, 'base64');
+    if (!fs.existsSync(DIR)) {
+      fs.mkdirSync(DIR);
+    }
+    fs.writeFileSync(path, data, 'base64');
+    console.log(`Saved screenshot: ${path}`);
   }
 }
