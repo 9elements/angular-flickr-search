dfcf4ea2309946172f07f307b217a9378ed2bc0b
Mathias Schaefer (molily) <molily@mailbox.org>
2021-06-29 23:27:10 +0200

    Move Cypress type declaration to commands.ts

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -44,6 +44,23 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Get one or more DOM elements by test id.
+     *
+     * @param id The test id
+     * @param options The same options as cy.get
+     */
+    byTestId<E extends Node = HTMLElement>(
+      id: string,
+      options?: Partial<
+        Cypress.Loggable & Cypress.Timeoutable & Cypress.Withinable & Cypress.Shadow
+      >,
+    ): Cypress.Chainable<JQuery<E>>;
+  }
+}
+
 Cypress.Commands.add(
   'byTestId',
   // Borrow the signature from cy.get
@@ -54,3 +71,4 @@ Cypress.Commands.add(
     >,
   ): Cypress.Chainable<JQuery<E>> => cy.get(`[data-testid="${id}"]`, options),
 );
+
