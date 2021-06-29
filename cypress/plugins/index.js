974fd373777c40fab13a66897ec9e19d48d56619
Mathias Schaefer (molily) <molily@mailbox.org>
2021-06-29 23:16:29 +0200

    Update to official cypress schematic

--- cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
-
-// Plugins taken from
-// https://github.com/briebug/cypress-schematic/tree/master/src/schematics/cypress/files/cypress/plugins
-// MIT License – Copyright (c) 2020 BrieBug Software
-
-module.exports = (on) => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor);
-};
